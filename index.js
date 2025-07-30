58e08cf13eb5f4c8f665812a4f7e89b0d7d700cd
Mvinit2245 <vinitmorankar22@gmail.com>
2025-07-30 10:22:59 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require("http");
 
 const server = http.createServer(function(req, res){
-    res.write("This is the firts server ! Created By viit morankar");
-    res.end();
+    //res.write("This is the firts server ! Created By viit morankar");
+      //res.end();
+      
+    if(req.url === "/"){
+         res.write("You are in the Home page");
+        res.end();
+    }else if (req.url === "/about"){
+        res.write("You are in the about page");
+        res.end();
+    }else{
+         res.write("Page Not Found");
+         res.end();
+    }
+    
 });
 
 server.listen(3000, function(){
